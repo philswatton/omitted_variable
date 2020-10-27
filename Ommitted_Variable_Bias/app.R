6d7c33bf7b38e784194b40efa6e4560cdf36a648
Phil Swatton <philswatton@gmail.com>
2020-10-27 22:51:19 +0000

    Attempted to construct server based on code, failed

--- Ommitted_Variable_Bias/app.R
+++ Ommitted_Variable_Bias/app.R
@@ -80,19 +80,63 @@ omit_graph(corrxz=0.5, bx=2, bz=2)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-    
+    numericInput("bx", "Beta x",value=2),
+    numericInput("bz", "Beta z",value=2),
+    numericInput("b0", "Intercept",value=5),
+    numericInput("corrXZ","Correlation(x,z)", min=-1, max=1,value=0.5)
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
+    
+    data <- reactive({rnorm_multi(n = 1000,
+                                  vars = 3,
+                                  mu = c(0,0,0),
+                                  sd = c(1,1,1),
+                                  r = c(1,input$corrXZ,0,
+                                        input$corrXZ,1,0,
+                                        0,0,1),
+                                  varnames = c("x","z","u")) %>%
+            mutate(y = input$b0 + input$bx*x + input$bz*z + u)
+        })
+    
+    model1 <- reactive({y ~ x})
+    model2 <- reactive({y ~ x + z})
+    modelList <- reactive({list(model1(), model2())})
+    modelOut <- reactive({map(modelList, ~lm(.x,data=data()))})
+    summary1 <- reactive({summary(modelOut()[[1]])})
+    summary2 <- reactive({summary(modelOut()[[2]])})
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+    output$biasPlot <- renderPlot({
+        ggplot(data = data(), mapping = aes(x = x, y = y)) +
+            geom_point(alpha = 0) + 
+            
+            #geom_abline(intercept = intercept, slope = bx, size = 1, color = "orange") +
+            
+            geom_abline(intercept = summary1()$coefficients[1,1], slope = summary1()$coefficients[2,1], size = 1, color = "red") +
+            
+            geom_abline(intercept = summary2()$coefficients[1,1], slope = summary2()$coefficients[2,1], size = 1, color = "blue") +
+            
+            xlim(0,NA) +
+            ylim(0,NA) +
+            
+            
+            annotate("text",
+                     x = 2,
+                     y = summary1()$coefficients[1,1] + 2 * summary1()$coefficients[2,1],
+                     #angle = atan(summary1$coefficients[2,1] * data$x_to_y)  * 180/pi,
+                     label = str_c("Biased Estimate: ", round(summary1()$coefficients[2,1], digits=2))) +
+            
+            
+            annotate("text",
+                     x = 2,
+                     y = summary2()$coefficients[1,1] + 2 * summary2()$coefficients[2,1],
+                     #angle = atan(summary1$coefficients[2,1] * data$x_to_y)  * 180/pi,
+                     label = str_c("Unbiased Estimate: ", round(summary2()$coefficients[2,1], digits=2))) +
+            theme_bw()
+        
+        return(out)
     })
 }
 
