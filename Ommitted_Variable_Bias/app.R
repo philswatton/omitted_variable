0834b83c55f827db9e3322bd5925c8bf049d39b1
Phil Swatton <philswatton@gmail.com>
2020-10-28 13:15:40 +0000

    removed superfluous code, set ylim to -4,4; xlim to 0,4

--- Ommitted_Variable_Bias/app.R
+++ Ommitted_Variable_Bias/app.R
@@ -1,96 +1,11 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+#### Phil Swatton - University of Essex/UCL ####
+# Shiny app designed to let students visualise the effect of ommitted variable bias
 
+# packages used:
 library(shiny)
 library(faux)
 library(tidyverse)
 library(DiagrammeR)
-
-
-omit_graph <- function (corrxz = 0.4, intercept = 5, bx = 2, bz = 2) {
-    
-    data <- rnorm_multi(n = 1000,
-                        vars = 3,
-                        mu = c(0,0,0),
-                        sd = c(1,1,1),
-                        r = c(1,corrxz,0,
-                              corrxz,1,0,
-                              0,0,1),
-                        varnames = c("x","z","u"))
-    
-    data$y <- intercept + bx*data$x + bz*data$z + data$u
-    
-    model1 <- y ~ x
-    model2 <- y ~ x + z
-    
-    modelList <- list(model1, model2)
-    
-    modelOut <- map(modelList, ~lm(.x,data=data))
-    
-    
-    summary1 <- summary(modelOut[[1]])
-    summary2 <- summary(modelOut[[2]])
-    
-    
-    out <- ggplot(data = data, mapping = aes(x = x, y = y)) +
-        geom_point(alpha = 0) + 
-        
-        #geom_abline(intercept = intercept, slope = bx, size = 1, color = "orange") +
-        
-        geom_abline(intercept = summary1$coefficients[1,1], slope = summary1$coefficients[2,1], size = 1, color = "red") +
-        
-        geom_abline(intercept = summary2$coefficients[1,1], slope = summary2$coefficients[2,1], size = 1, color = "blue") +
-        
-        xlim(0,NA) +
-        ylim(0,NA) +
-        
-        
-        annotate("text",
-                 x = 2,
-                 y = summary1$coefficients[1,1] + 2 * summary1$coefficients[2,1],
-                 #angle = atan(summary1$coefficients[2,1] * data$x_to_y)  * 180/pi,
-                 label = str_c("Biased Estimate: ", round(summary1$coefficients[2,1], digits=2))) +
-        
-        
-        annotate("text",
-                 x = 2,
-                 y = summary2$coefficients[1,1] + 2 * summary2$coefficients[2,1],
-                 #angle = atan(summary1$coefficients[2,1] * data$x_to_y)  * 180/pi,
-                 label = str_c("Unbiased Estimate: ", round(summary2$coefficients[2,1], digits=2))) +
-        theme_bw()
-    
-    return(out)
-}
-
-omit_graph(corrxz=0.5, bx=2, bz=2)
-
-
-grViz(str_c("digraph {
-
-  # a 'graph' statement
-  graph [layout = dot,
-       rankdir = LR,
-  overlap = true,
-  fontsize = 10]
-
-  # several 'node' statements
-  node [shape = circle]
-  X, Y, Z
-
-  # several 'edge' statements
-  X->Y[label = 'bx: ",1,"', color = red, fontcolor = red]
-  Z->Y[label = 'bz: ",1,"', color = orange, fontcolor = orange]
-  X->Z[dir=both, label = 'corr(x,z): ",0.5,"', color = blue, fontcolor = blue]
-}"))
-
-
-# biased result: E[bx] = bx + c * bz
 
 
 # Define frontend UI for application
@@ -124,7 +39,7 @@ ui <- fluidPage(
 
 
 # Define backend server logic for application
-server <- function(input, output, session) {
+server <- function(input, output) {
     
     output$formulas <- renderUI({
         withMathJax(
@@ -202,8 +117,8 @@ server <- function(input, output, session) {
             
             geom_abline(intercept = summary2$coefficients[1,1], slope = summary2$coefficients[2,1], size = 1, color = "blue") +
             
-            xlim(0,NA) +
-            #ylim(0,NA) +
+            xlim(0,4) +
+            ylim(-4,4) +
             
             
             annotate("text",
