c551986bd0ba45fa98e0905fb49a7ec32c76f847
Phil Swatton <philswatton@gmail.com>
2020-10-28 14:21:47 +0000

    init commit

---
+++ app_deploy.R
@@ -0,0 +1,5 @@
+#### Phil Swatton - University of Essex/UCL ####
+# Script for deploying shiny app designed to let students visualise the effect of omitted variable bias
+
+library(rsconnect)
+rsconnect::deployApp('C:\\Users\\User\\Documents\\Projects\\omitted_variable')
