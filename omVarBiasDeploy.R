84241f4f7ee78c4a7f9ec0321530a4594b6040ed
Phil Swatton <philswatton@gmail.com>
2020-10-28 13:26:48 +0000

    init commit

---
+++ omVarBiasDeploy.R
@@ -0,0 +1,10 @@
+#### Phil Swatton - University of Essex/UCL ####
+# Script for deploying shiny app designed to let students visualise the effect of ommitted variable bias
+
+library(rsconnect)
+
+rsconnect::setAccountInfo(name='philswatton',
+                          token='AA02538D4AA16CE67C7E38E72DB0A466',
+                          secret='<SECRET>')
+
+rsconnect::deployApp('omVarBiasApp.R')
